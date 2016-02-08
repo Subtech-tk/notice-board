aaf2ed5b2c80434492bd209e56a3c554a5e1727e
Amanskywalker <ak47su30ac@gmail.com>
2016-02-08 19:46:52 +0530

    some fixes
    	modified:   add_notice.php
    	new file:   functions/category_list.func.php

---
+++ functions/category_list.func.php
@@ -0,0 +1,29 @@
+<?php
+	// category_list.func.php
+	
+	/*
+		this function defines the array which contains the array of categories defined in databse 
+		
+		output = array 
+	*/
+	function category_list()
+	{
+		$array = array('Birthday','General','Placement');
+		
+		asort($array);
+		
+		return $array;
+	}
+
+	/*
+		this function defines the array which contains the pirorty 
+	*/
+	function pirorty_list()
+	{
+		$array = array('Not at all Important','Little bit Important','Very Much Important');
+		
+		asort($array);
+		
+		return $array;
+	}
+?>
